92af16f4d2c90b4a9bc8f233a232b2bf87b7ee01
Naveen <108810648+nv19090@users.noreply.github.com>
2026-06-10 05:58:47 +0530

    Create 2_Add_Two_Numbers_Linked_List.java

---
+++ LinkList/2_Add_Two_Numbers_Linked_List.java
@@ -0,0 +1,75 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverse(ListNode head) {
+        ListNode curr = head;
+        ListNode prev = null;
+        ListNode next = null;
+
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        return prev;
+    }
+
+    public ListNode result(ListNode l3, int sum) {
+        ListNode newNode = new ListNode(sum);
+        if (l3 == null) {
+            return newNode;
+        }
+        newNode.next = l3;
+        l3 = newNode;
+        return l3;
+    }
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode l3 = null;
+        int carry = 0;
+        int sum = 0;
+
+        while (l1 != null && l2 != null) {
+            sum = l1.val + l2.val+carry;
+            carry = sum / 10;
+            sum = sum % 10;
+            l3 = result(l3, sum);
+            
+            l1 = l1.next;
+            l2 = l2.next;
+        }
+        while (l2 != null) {
+            sum = l2.val + carry;
+
+            carry = sum / 10;
+            sum = sum % 10;
+
+            l3 = result(l3, sum);
+            l2 = l2.next;
+        }
+        while (l1 != null) {
+            sum = l1.val + carry;
+
+            carry = sum / 10;
+            sum = sum % 10;
+
+            l3 = result(l3, sum);
+            l1 = l1.next;
+        }
+        if (carry > 0) {
+            l3 = result(l3, carry);
+        }
+        l3 = reverse(l3);
+        return l3;
+    }
+}
