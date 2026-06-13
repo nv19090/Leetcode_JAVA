fdd7c0bd734ef45e9799cc8697c415ae1e145d96
Naveen <108810648+nv19090@users.noreply.github.com>
2026-06-13 16:33:13 +0530

    Create 3689_Maximum_Total_Subarray_Value_I.java

---
+++ Arrays_Basics/3689_Maximum_Total_Subarray_Value_I.java
@@ -0,0 +1,13 @@
+class Solution {
+    public long maxTotalValue(int[] nums, int k) {
+        int n=nums.length;
+        int mn=Integer.MAX_VALUE;
+        int mx=Integer.MIN_VALUE;
+        for(int i=0;i<n;i++){
+            mn=Math.min(mn,nums[i]);
+            mx=Math.max(mx,nums[i]);
+        }
+        long answer=mx-mn;
+        return k*answer;
+    }
+}
