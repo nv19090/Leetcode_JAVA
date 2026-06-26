9f1610f21ce0609ed13efbbbd5dc0073d8669672
Naveen <108810648+nv19090@users.noreply.github.com>
2026-06-27 02:32:19 +0530

    237_Delete_Node_in_a_Linked_List.java

--- Linked_List/237_Delete_Node_in_a_Linked_List.java
+++ Linked_List/237_Delete_Node_in_a_Linked_List.java
@@ -2,6 +2,6 @@ class Solution {
     public void deleteNode(ListNode node) {
         ListNode nextNode = node.next;
         node.val = nextNode.val;
-        node.next = nextNode.next;
+        node.next = nextNode.next; 
     }
 }
