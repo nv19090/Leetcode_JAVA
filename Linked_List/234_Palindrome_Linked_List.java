0caf50d5ca213821684fb1e531c5a6148cb2cddd
Naveen <108810648+nv19090@users.noreply.github.com>
2026-06-27 02:32:07 +0530

    234_Palindrome_Linked_List.java

--- Linked_List/234_Palindrome_Linked_List.java
+++ Linked_List/234_Palindrome_Linked_List.java
@@ -8,7 +8,7 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-class Solution {
+class Solution { 
 public:
     bool isPalindrome(ListNode* head) {
         stack<int> s;
