ac589b944c139d4454eda23f5d3364d97fb2a5a1
Naveen <108810648+nv19090@users.noreply.github.com>
2026-06-27 02:34:25 +0530

    Update 238._Product_of_Array_Except_Self.java

--- Prefix_Sum/238._Product_of_Array_Except_Self.java
+++ Prefix_Sum/238._Product_of_Array_Except_Self.java
@@ -7,7 +7,7 @@ class Solution {
 
         int[] suffix = new int[n];
 
-        suffix[n - 1] = 1;
+        suffix[n - 1] = 1; 
 
         for (int i = n - 2; i >= 0; i--) {
             suffix[i] = suffix[i + 1] * nums[i + 1];
