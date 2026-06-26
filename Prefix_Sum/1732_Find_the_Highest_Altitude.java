0dbe60ab954e8973b094e24a6dbe5a61797a8c74
Naveen <108810648+nv19090@users.noreply.github.com>
2026-06-27 02:34:17 +0530

    Update 1732_Find_the_Highest_Altitude.java

--- Prefix_Sum/1732_Find_the_Highest_Altitude.java
+++ Prefix_Sum/1732_Find_the_Highest_Altitude.java
@@ -4,7 +4,7 @@ class Solution {
         int sum=0;
         int mx=0;
         for(int i=1;i<n+1;i++){
-            sum+=gain[i-1];
+            sum+=gain[i-1]; 
             mx=Math.max(mx,sum);
         }
         return mx;
