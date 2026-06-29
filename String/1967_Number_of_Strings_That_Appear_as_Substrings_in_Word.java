0062528a198bbdbf53380b998b98b5539be7c14b
Naveen <naveenpabnawa82@gmail.com>
2026-06-29 16:26:58 +0530

    Solved_1967_Number_of_Strings_That_Appear_as_Substrings_in_Word.java

--- String/1967_Number_of_Strings_That_Appear_as_Substrings_in_Word.java
+++ String/1967_Number_of_Strings_That_Appear_as_Substrings_in_Word.java
@@ -3,7 +3,7 @@ class Solution {
         int n = patterns.length;
         int count=0;
         for (int i = 0; i < n; i++) {
-            if(word.contains(patterns[i])){
+            if (word.contains(patterns[i])) {
                 count++;
             }
         }
