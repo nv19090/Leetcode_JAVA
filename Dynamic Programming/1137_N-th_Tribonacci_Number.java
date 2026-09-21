7cba6f514c6c954ba26bb34d6685b78cc28ce931
Naveen <108810648+nv19090@users.noreply.github.com>
2026-09-21 18:13:36 +0530

    Update 1137_N-th_Tribonacci_Number.java

--- Dynamic Programming/1137_N-th_Tribonacci_Number.java
+++ Dynamic Programming/1137_N-th_Tribonacci_Number.java
@@ -3,8 +3,7 @@ class Solution {
         return tribonacci(n,new HashMap<>());
     }
     public int tribonacci(int n,HashMap<Integer,Integer> mp){
-        if(n==0) return 0;
-        else if(n<=2) return 1;
+        if(n<=3) return n;
         if(mp.containsKey(n)) return mp.get(n);
         int result=tribonacci(n-1,mp)+tribonacci(n-2,mp)+tribonacci(n-3,mp);
         mp.put(n,result);
